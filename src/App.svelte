a1762428f38e2eeefccb792a15134c2adceb47c7
Harish Kumar Gangula <harish.kumar@ennoventure.com>
2022-09-12 17:27:39 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<svelte:options tag="sunbird-pdf-player" />
+<script lang="ts">
+  let src = "src/assets/pdfjs/web/viewer.html?file=https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_213427882013302784110014/artifact/pdf_222.pdf"
+</script>
+
+<main>
+ 
+  <iframe frameborder="0" src={src} title="player"></iframe>
+</main>
+
+<style>
+  p {
+    color: brown;
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 2rem;
+  }
+</style>
+
